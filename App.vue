829dc3acda1b829a6bf4f85a373b8ecc258b6ced
Nedim Kulasin <nedim.kulasin@teneo.ba>
2024-04-19 11:04:23 +0200

    API POZIV NAPRAVLJEN

--- App.vue
+++ App.vue
@@ -1,51 +1,54 @@
 <template>
- <head>
-    <div id="app">
-    </div>
-  </head>     
+  <div id="app">
     <main>
-         <div class="search-box">
+      <div class="search-box">
         <input 
-        type="text" class="search-bar" placeholder="Search..."
-        v-model:"query"
+          type="text" class="search-bar" placeholder="Search..."
+          v-model="query"
         />
-        {{query}}
-        </div>
+        {{ query }}
+      </div>
       
-        <div class="weather-wrap">
-         <div class="location-box">
-           <div class="location">Sarajevo, BiH</div>
-              <div class="date">17 April 2024 </div>
-          </div>
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Sarajevo, BiH</div>
+          <div class="date">17 April 2024 </div>
         </div>
+      </div>
 
       <div class="weather-box">
         <div class="temp">10°C</div>
         <div class="weather">Kišovito</div>
-    
       </div>
-
-  </main>
+    </main>
+  </div>
 </template>
 
-     
 <style>
-  @import ' ./src/assets/main.css ';
+ /* @import '/main.css';*/
 </style>
 
 <script>
-export deafult{
+export default {
   name: 'app',
-  Vue.createApp ({
   data() {
     return {
-      api_key: 'b0fbe4aff43d7f282e2c196d21722a79'
-      url_base: 'https://home.openweathermap.org/'
-
+      api_key: 'b0fbe4aff43d7f282e2c196d21722a79',
+      url_base: 'https://home.openweathermap.org/',
+      query: ''
+    };
+  },
+  mounted() {
+    this.callWeatherAPI();
+  },
+  methods: {
+    callWeatherAPI() {
+      const apiUrl = `${this.url_base}?apikey=${this.api_key}&query=${this.query}`;
+      console.log('API URL:', apiUrl);
+      
+      // Ovdje uradi Axios ili Fetch request dalje, to istraži malo
     }
   }
-  })
-}
-
-
+};
 </script>
+
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import './src/assets/main.css'
+/*import './src/assets/main.css'*/
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '/App.vue'
 
 createApp(App).mount('#app')
