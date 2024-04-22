740016e8cc79725235b86695ee8f503f91493baf
Adna Sisic <adna.sisic99@icloud.com>
2024-04-22 11:47:01 +0200

    project finished

--- App.vue
+++ App.vue
@@ -5,50 +5,47 @@
         <input 
           type="text" class="search-bar" placeholder="Search..."
           v-model="query"
+          @keypress.enter="fetchWeather"
         />
-        {{ query }}
+        
       </div>
       
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'"> 
         <div class="location-box">
-          <div class="location">Sarajevo, BiH</div>
-          <div class="date">17 April 2024 </div>
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ new Date(weather.dt * 1000).toLocaleDateString() }}</div>
         </div>
-      </div>
-
-      <div class="weather-box">
-        <div class="temp">10°C</div>
-        <div class="weather">Kišovito</div>
+        <div class="weather-box">
+          <div class="temp">{{ Math.round (weather.main.temp) }} °C</div>
+          <div class="weather">{{ weather.weather[0].description }}</div>
+        </div>
       </div>
     </main>
   </div>
 </template>
-
-<style>
- /* @import '/main.css';*/
-</style>
 
 <script>
 export default {
   name: 'app',
   data() {
     return {
-      api_key: 'b0fbe4aff43d7f282e2c196d21722a79',
-      url_base: 'https://home.openweathermap.org/',
-      query: ''
+      api_key: 'b6a0aec6e8bdc722d0d753f0b224c2da',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
     };
   },
-  mounted() {
-    this.callWeatherAPI();
-  },
   methods: {
-    callWeatherAPI() {
-      const apiUrl = `${this.url_base}?apikey=${this.api_key}&query=${this.query}`;
-      console.log('API URL:', apiUrl);
-      
-      // Ovdje uradi Axios ili Fetch request dalje, to istraži malo
+    fetchWeather() {
+      fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        .then(res => res.json())
+        .then(data => {
+          this.weather = data;
+        })
+        .catch(err => {
+          console.error('Error fetching weather data:', err);
+        });
     }
   }
-};
+}
 </script>
-
