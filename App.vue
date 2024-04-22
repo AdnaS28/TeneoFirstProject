a561ebf66f57a29226fb729f54f15d4a129dc337
Adna Sisic <adna.sisic99@icloud.com>
2024-04-22 14:04:45 +0200

    5-day forecast created

--- App.vue
+++ App.vue
@@ -7,17 +7,13 @@
           v-model="query"
           @keypress.enter="fetchWeather"
         />
-        
       </div>
       
-      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'"> 
-        <div class="location-box">
-          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
-          <div class="date">{{ new Date(weather.dt * 1000).toLocaleDateString() }}</div>
-        </div>
-        <div class="weather-box">
-          <div class="temp">{{ Math.round (weather.main.temp) }} °C</div>
-          <div class="weather">{{ weather.weather[0].description }}</div>
+      <div v-if="weather.forecast" class="weather-wrap">
+        <div v-for="day in weather.forecast" :key="day.dt" class="forecast-box">
+          <div class="date">{{ new Date(day.dt * 1000).toLocaleDateString() }}</div>
+          <div class="weather">{{ day.weather[0].description }}</div>
+          <div class="temp">{{ Math.round(day.main.temp) }} °C</div>
         </div>
       </div>
     </main>
@@ -37,14 +33,28 @@ export default {
   },
   methods: {
     fetchWeather() {
-      fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+      fetch(`${this.url_base}forecast?q=${this.query}&units=metric&APPID=${this.api_key}`)
         .then(res => res.json())
         .then(data => {
-          this.weather = data;
+          this.weather = {
+            forecast: this.extractFiveDayForecast(data)
+          };
         })
         .catch(err => {
           console.error('Error fetching weather data:', err);
         });
+    },
+    extractFiveDayForecast(data) {
+      const forecasts = data.list.filter(item => {
+        // Selecting forecast data for 12:00 PM of each day
+        return item.dt_txt.includes('12:00:00');
+      });
+
+      return forecasts.map(item => ({
+        dt: item.dt,
+        main: item.main,
+        weather: item.weather
+      }));
     }
   }
 }
